25f432005d823fa65378df16539cadacd141f1b3
Dani <dpajaresfo@gmail.com>
2025-05-14 19:28:12 +0200

    añadimos las funciones de actualizar display, introducir numeros

--- js/funciones.js
+++ js/funciones.js
@@ -12,18 +12,54 @@ document.addEventListener('DOMContentLoaded', () => {
     botonMostrar.textContent = calculadoraContainer.classList.contains('show') ? 'Ocultar Calculadora' : 'Mostrar Calculadora';
     });
 
+    //variables
 
+    const display = document.getElementById('display');
+    const botones = document.querySelectorAll('.botones button');
 
-
-
-
-
-
-
-
-
-
-
+    let entradaActual = '0';      // Lo que se muestra en la pantalla
     
+    // para actualizar el display
+    function updateDisplay() {
+        display.textContent = entradaActual;
+    }
+
+    // para introducir los numeros 
+    function introducirNumero(num) {
+    if (entradaActual === '0' || entradaActual === 'Error') {
+        entradaActual = num;
+    } else {
+        entradaActual += num;
+    }
+    updateDisplay();
+    }
+    
+    // para asignar los numeros a los botones
+    botones.forEach(btn => {
+        const id = btn.id;
+        const classList = btn.classList;
+
+        if (id >= '0' && id <= '9') {
+            // Números
+            btn.addEventListener('click', () => {
+                introducirNumero(id);
+            });       
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
